62970e10e801fa678299da4c75d25390d6e956a1
Satvika1 <98373372+Satvika1@users.noreply.github.com>
2022-02-09 12:52:07 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,26 @@
+package week3.day1.assignment3;
+
+public class BankInfo {
+public void savings() {
+	System.out.println("Minimum Balance : \t Rs.1000");
+	
+}
+public void fixed() {
+	System.out.println("Fixed Deposit:"+" \t "+"Rs.5000");
+		
+	}
+
+public void deposit() {
+	System.out.println("Deposit Ammount is:\t Rs.5000");
+
+
+
+
+}
+
+
+
+
+
+
+}
